b1a4629eef64f75c06f83842b87021530e21ff20
Tanchwa <dres2dogs@gmail.com>
2022-02-15 20:25:02 -0500

    fixed distribute-keys script

--- ansible/distribute-keys.sh
+++ ansible/distribute-keys.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#you need to make sure this is executale by all users, else itll try to run as root which doesn't have any stored keys#
+
 FILE=$1
 egrep "([[:digit:]]{1,3}\.?){4}|^[^\[]" $FILE | awk '{print $1}' | while read SERVERNAME;
     do ssh-copy-id $SERVERNAME
