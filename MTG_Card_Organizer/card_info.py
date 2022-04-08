90230dcec95e05737e7789146f7d4086efa73925
Tanchwa <dres2dogs@gmail.com>
2022-04-08 19:35:18 -0400

    debugging user input

--- MTG_Card_Organizer/card_info.py
+++ MTG_Card_Organizer/card_info.py
@@ -73,10 +73,8 @@ if __name__ == "__main__":
         test_script(multiverse_id)
 
 
-    user_input = ""
-    while user_input != "exit":
-        user_input = input("Hi, Ethan! Try Me! Enter a card and set number to see its price!\n(formated in card_name, set_name):\nEnter 'Exit' to quit\n>")
-        card_name, set_name = user_input.split(", ")
-        card_price = card_price_lookup(card_name, set_name)
-        print(f"Your Card {card_name} from {set_name} is {card_price}")
+    user_input = input("Hi, Ethan! Try Me! Enter a card and set number to see its price!\n(formated in card_name, set_name):\nEnter 'Exit' to quit\n>")
+    card_name, set_name = user_input.split(", ")
+    card_price = card_price_lookup(card_name, set_name)
+    print(f"Your Card {card_name} from {set_name} is {card_price}")
 
