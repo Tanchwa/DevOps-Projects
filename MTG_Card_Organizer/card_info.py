93bf1856d229178b1e1252813d6f713da72dd82a
Tanchwa <dres2dogs@gmail.com>
2022-05-20 21:27:49 +0100

    edited user input for null occurances

--- MTG_Card_Organizer/card_info.py
+++ MTG_Card_Organizer/card_info.py
@@ -79,6 +79,9 @@ if __name__ == "__main__":
     user_input = ""
     while user_input != "exit":
         user_input = input("Hi, Thomas! Try Me! Enter a card and set number to see its price!\n(formated in card_name, set_name):\nType 'exit' to exit\n>")
-        card_name, set_name = user_input.split(", ")
-        card_price = card_price_lookup(card_name, set_name)
-        print(f"Your Card {card_name} from {set_name} is {card_price}\nEnter another card, or type 'exit' to exit")
+        if type(input) == tuple:
+            card_name, set_name = user_input.split(", ")
+            card_price = card_price_lookup(card_name, set_name)
+            print(f"Your Card {card_name} from {set_name} is {card_price}\nEnter another card, or type 'exit' to exit")
+        else:
+            print("Please Enter a card name AND set name, or type exit to exit")
