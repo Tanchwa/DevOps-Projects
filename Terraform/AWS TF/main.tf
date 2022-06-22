1121cb3240697dba6394e0b985b4db13e1d23ba0
Tanchwa <dres2dogs@gmail.com>
2022-06-22 14:49:39 -0400

    + CDN draft for ansible, + ansible tools

--- Terraform/AWS TF/main.tf
+++ Terraform/AWS TF/main.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "preprocessing" {
     }
 }
 resource "aws_s3_bucket_acl" "preprocessing_acl" {
-  bucket = aws_s3_bucket.preprocessing.bucket
+  bucket = aws_s3_bucket.preprocessing.id
   acl    = "private"
 }
 resource "aws_s3_bucket" "postprocessing" {
@@ -33,7 +33,7 @@ resource "aws_s3_bucket" "postprocessing" {
     }
 }
 resource "aws_s3_bucket_acl" "postprocessing_acl" {
-  bucket = aws_s3_bucket.postprocessing.bucket
+  bucket = aws_s3_bucket.postprocessing.id
   acl    = "private"
 }
 resource "aws_s3_bucket" "webpage" {
@@ -43,7 +43,7 @@ resource "aws_s3_bucket" "webpage" {
     }
 }
 resource "aws_s3_bucket_website_configuration" "webpage_config" {
-    bucket = aws_s3_bucket.webpage.bucket
+    bucket = aws_s3_bucket.webpage.id
     index_document {
         suffix = "index.html"
     }
@@ -52,7 +52,7 @@ resource "aws_s3_bucket_website_configuration" "webpage_config" {
     }
 }
 resource "aws_s3_bucket_acl" "webpage_acl" {
-  bucket = aws_s3_bucket.webpage.bucket
+  bucket = aws_s3_bucket.webpage.id
   acl    = "private"
 }
 
@@ -95,7 +95,78 @@ resource "aws_lambda_function" "video_encoder" {
     runtime         = "python3.9"
 }
 */
+
 #cloudfront distribution
 #further reading 
 #https://aws.amazon.com/blogs/networking-and-content-delivery/serving-compressed-webgl-websites-using-amazon-cloudfront-amazon-s3-and-aws-lambda/
 #https://www.youtube.com/watch?v=mls8tiiI3uc
+
+#things we need, origin domain id from bucket, protocol policy, caching behavior
+resource "aws_cloudfront_distribution" "s3_distribution" {
+    origin {
+        domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+        origin_id   = local.s3_origin_id
+
+        s3_origin_config {
+            origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+        }
+    } 
+
+    enabled             = true
+    is_ipv6_enabled     = true
+    comment             = "Some comment"
+    default_root_object = "index.html"
+
+    logging_config {
+        include_cookies = false
+        bucket          = "mylogs.s3.amazonaws.com"
+        prefix          = "myprefix"
+    }
+
+    aliases = []
+  
+    default_cache_behavior {
+        allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods   = ["GET", "HEAD"]
+        target_origin_id = local.s3_origin_id
+
+        forwarded_values {
+            query_string = false
+
+            cookies {
+                forward = "none"
+            }
+        }
+
+        viewer_protocol_policy = "allow-all"
+        min_ttl                = 0
+        default_ttl            = 3600
+        max_ttl                = 86400
+    }
+
+    # Cache behavior with precedence 0 
+    #we need to cache the videos... figure out where videos are
+    ordered_cache_behavior {
+        #do I select videos directory here? 
+        path_pattern     = "/content/immutable/*"
+        allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+        cached_methods   = ["GET", "HEAD", "OPTIONS"]
+        target_origin_id = local.s3_origin_id
+
+        forwarded_values {
+            #can I use this to forward search queries to a DB?
+            query_string = false
+            headers      = ["Origin"]
+
+            cookies {
+                forward = "none"
+            }
+        }
+
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl                = 0
+        default_ttl            = 86400
+        max_ttl                = 31536000
+        compress               = true
+    }
+}
