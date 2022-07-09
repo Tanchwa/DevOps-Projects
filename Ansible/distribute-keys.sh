49fd95beba7060df39042c4e44f96ba2166912f3
Tanchwa <dres2dogs@gmail.com>
2022-07-08 23:18:24 -0400

    init role for apache, troubleshooting template

--- Ansible/distribute-keys.sh
+++ Ansible/distribute-keys.sh
@@ -3,7 +3,8 @@
 #this is designed to work with the layout of ansible inventory files#
 #you need to make sure this is executale by all users, else itll try to run as root which doesn't have any stored keys#
 
-FILE=$1
-egrep "([[:digit:]]{1,3}\.?){4}|^[^\[]" $FILE | awk '{print $1}' | while read SERVERNAME;
-    do ssh-copy-id $SERVERNAME
+INVENTORY_FILE=$1
+KEYFILE=$2
+egrep "([[:digit:]]{1,3}\.?){4}|^[^\[]" $INVENTORY_FILE | awk '{print $1}' | while read SERVERNAME;
+    do ssh-copy-id -i $KEYFILE $SERVERNAME
 done
